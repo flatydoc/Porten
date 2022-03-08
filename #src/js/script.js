185154a363ac54bb30347f14f8df5a2b6cab3837
Владислав <flatydoc@gmail.com>
2022-03-09 00:24:55 +0200

    add modal

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,4 +1,13 @@
-var button = document.querySelector(".header__burger");
+var burgerButton = document.querySelector(".header__burger-button");
+var sidebar = document.querySelector(".sidebar");
+var searchButton = document.querySelector(".header__search-button");
+var search = document.querySelector(".header__search");
+var userLink = document.querySelector(".header__navigation-user");
+var modalLogin = document.querySelector(".modal__user");
+var modalForm = document.querySelector(".modal__form");
+var modalClose = document.querySelector(".modal-close");
+var login = modalLogin.querySelector("[name=login]");
+var password = modalLogin.querySelector("[name=password]");
 
 function testWebP(callback) {
   var webP = new Image();
@@ -17,7 +26,63 @@ testWebP(function (support) {
   }
 });
 
-button.addEventListener("click", function (evt) {
+burgerButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  button.classList.toggle("header__burger-close");
+  burgerButton.classList.toggle("header__burger-close");
+  sidebar.classList.toggle("sidebar-show");
+});
+
+search.addEventListener("click", function () {
+  searchButton.classList.add("header__search-show");
+});
+
+var isStorageSupport = true;
+var storage = " ";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+userLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalLogin.classList.toggle("modal__user-show");
+  login.focus();
+  if (storage) {
+    login.value = storage;
+    password.focus();
+  } else {
+    login.focus();
+  }
+});
+
+modalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalLogin.classList.remove("modal__user-show");
+  modalLogin.classList.remove("modal-error");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalLogin.classList.contains("modal__user-show")) {
+      evt.preventDefault();
+      modalLogin.classList.remove("modal__user-show");
+      modalLogin.classList.remove("modal-error");
+    }
+  }
+});
+
+modalForm.addEventListener("submit", function (evt) {
+  if (!login.value || !password.value) {
+    evt.preventDefault();
+    modalLogin.classList.remove("modal-error");
+    modalLogin.offsetWidth = modalLogin.offsetWidth;
+    modalLogin.classList.add("modal-error");
+    console.log("Нужно ввести логин и пароль");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }
+  }
 });
